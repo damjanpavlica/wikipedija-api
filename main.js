a49ddb5c7a23b1f8c907f5b43d60671abeb21444
damjanpavlica <44097267+damjanpavlica@users.noreply.github.com>
2019-01-29 19:29:09 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-const trazenaRec = "The Cure"
-const url = `https://en.wikipedia.org/w/api.php?action=query&titles=${trazenaRec}&prop=extracts|pageimages|info&pithumbsize=400&inprop=url&redirects=&format=json&origin=*`
+const dugme = document.getElementById('dugme');
+const trazeniPojam = document.getElementById('trazeni-pojam');
+const drzacZaClanak = document.getElementById('drzac-za-clanak');
+const naslov = document.getElementById('naslov');
 
-fetch(url)
-    .then(response => response.json())
-    .then(podatak => {
-        const pages = podatak.query.pages       
-    })
+function ucitajWiki() {
+    const trazenaRec = trazeniPojam.value;
+    const url = `https://en.wikipedia.org/w/api.php?action=query&titles=${trazenaRec}&prop=extracts|pageimages|info&pithumbsize=400&inprop=url&redirects=&format=json&origin=*`;
+    drzacZaClanak.innerHTML = '';
+    naslov.innerText = '';
+
+    fetch(url)
+        .then(response => response.json())
+        .then(podatak => {
+            const pages = podatak.query.pages;
+            const clanak = Object.values(pages)[0] // pretvara vrednosti objekta u niz
+            const imgSrc = clanak.thumbnail.source;
+    
+            naslov.innerText = clanak.title;
+            drzacZaClanak.innerHTML += `<img src="${imgSrc}" alt="${clanak.title}">`
+            drzacZaClanak.innerHTML += clanak.extract;
+        })
+}
+
+dugme.addEventListener('click', ucitajWiki)
